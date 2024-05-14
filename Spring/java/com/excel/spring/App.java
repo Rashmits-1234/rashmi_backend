2aaad63a0e76473df69b3c3d0fc2798d2ba0c855
Rashmi T S. <rashmi.ts@excelindia.com>
2024-05-14 10:50:58 +0530

    spring

---
+++ Spring/java/com/excel/spring/App.java
@@ -0,0 +1,13 @@
+package com.excel.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
