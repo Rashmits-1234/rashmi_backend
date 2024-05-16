1e5623830f0bebc490ba10002b37424ae6728e73
Rashmits-1234 <108087673+Rashmits-1234@users.noreply.github.com>
2024-05-16 18:41:47 +0530

    Delete lms/springboot_demo directory

--- lms/springboot_demo/src/main/java/com/excel/springboot_demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excel.springboot_demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.excel.springboot_demo.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
-
-}
